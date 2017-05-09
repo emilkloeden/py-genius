b5c8381a405b3bb8f8a640af9303daec2fbdb952
Emil Kloeden <emilkloeden@gmail.com>
2017-05-09 22:18:06 +0930

    Added some pypi paraphenalia

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'genius',
+  packages = ['genius'], # this must be the same as the name above
+  version = '0.1.0',
+  description = 'A thin wrapper around the Genius API',
+  author = 'Emil Kloeden',
+  author_email = 'emilkloeden@gmail.com',
+  url = 'https://github.com/emilkloeden/py-genius', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/emilkloeden/py-genius/0.1.0.tar.gz', # I'll explain this in a second
+  keywords = ['genius'], # arbitrary keywords
+  classifiers = [],
+)
