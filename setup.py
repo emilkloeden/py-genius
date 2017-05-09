3a2d27c89259e5fae4278e9a10008502276a04ba
Emil Kloeden <emilkloeden@gmail.com>
2017-05-09 22:36:51 +0930

    Fixed setup.py, now points to correct url

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from distutils.core import setup
 setup(
-  name = 'genius',
-  packages = ['genius'], # this must be the same as the name above
-  version = '0.1.0',
+  name = 'py-genius',
+  version = '0.2.0',
   description = 'A thin wrapper around the Genius API',
   author = 'Emil Kloeden',
   author_email = 'emilkloeden@gmail.com',
   url = 'https://github.com/emilkloeden/py-genius', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/emilkloeden/py-genius/0.1.0.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/emilkloeden/py-genius/0.2.0.tar.gz', # I'll explain this in a second
   keywords = ['genius'], # arbitrary keywords
   classifiers = [],
 )
